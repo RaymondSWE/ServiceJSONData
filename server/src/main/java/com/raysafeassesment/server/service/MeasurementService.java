e7e30771258261b44b8969a0328742e5aa1d436f
RaymondSWE <ramanassaredsskolan@hotmail.com>
2024-09-10 10:47:21 +0200

    service interface added

---
+++ server/src/main/java/com/raysafeassesment/server/service/MeasurementService.java
@@ -0,0 +1,8 @@
+package com.raysafeassesment.server.service;
+
+import com.raysafeassesment.server.model.MeasurementData;
+
+public interface MeasurementService {
+
+    boolean validationOfMeasurementData(MeasurementData data);
+}
